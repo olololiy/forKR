281cc4fba75e5363833052f567c05cb10c0b7f44
Artemy <64956500+olololiy@users.noreply.github.com>
2022-02-21 22:01:05 +0300

    теперь код выглядит красиво, и даже понятно

--- buildByJS/app.js
+++ buildByJS/app.js
@@ -2,7 +2,7 @@ let makeBlocks = (numberOfBlocks) => {
     let i = 1;
     let container = document.querySelector('.container')
     let hexagon = (i) => {
-        return`<div class="block" style="background-color: transparent;">
+        return `<div class="block" style="background-color: transparent;">
             <p class="num">${i}</p>
             <div class="hexagon">
                 <div class="hexagon__top"></div>
@@ -22,21 +22,13 @@ let makeBlocks = (numberOfBlocks) => {
         </div>`
     }
     let addBlock = (figure) => {
-        container.insertAdjacentHTML("beforeend",
-            figure)
+        container.insertAdjacentHTML("beforeend", figure)
     }
 
-    for (i = 1; i <= numberOfBlocks; i++){
-        if (i === 5){
-            addBlock(hexagon(i))
-        }
-        else if(i % 3 === 0){
-            addBlock(rectangleWithQuads(i))
-        }
-        else {
-            addBlock(rectangle(i))
-
-        }
-        }
+    for (i = 1; i <= numberOfBlocks; i++) {
+        (i === 5) ? addBlock(hexagon(i)) :
+            (i % 3 === 0) ? addBlock(rectangleWithQuads(i)) :
+                addBlock(rectangle(i))
+    }
 }
 makeBlocks(9);
